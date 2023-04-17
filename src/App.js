3e1fb8e1ac87bc5d54d2895a4a74701338f31100
Vaibhav Kumar <vaibhavkr87097@gmail.com>
2023-04-17 11:43:21 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Navbar from "./Components/Navbar/Navbar"
+import Hero from "./Components/Hero/Hero"
+
+function App() {
+  return (
+    <div className="app">
+   <Navbar/>
+   <Hero/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+export default function Index() {
+
+
+  return (
+
+    <div className="text-red-500 text-center text-xl hover:text-green-700 ">
+        <h1 className="font-semibold"> Hello</h1>
+       
+    </div>
+ 
+    );
+}
