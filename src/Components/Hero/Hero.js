5ff3d7bc6ee91f0e5a936a6f2b3916254fccf689
Vaibhav Kumar <vaibhavkr87097@gmail.com>
2023-04-17 11:46:59 +0530

    text change

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -6,7 +6,7 @@ export default function Index() {
   return (
 
     <div className="text-red-500 text-center text-xl hover:text-green-700 ">
-        <h1 className="font-semibold"> Hello</h1>
+        <h1 className="font-semibold"> Hello i am vaibhav</h1>
        
     </div>
  
