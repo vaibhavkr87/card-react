6e609b4a695b08eb8532cf661037740e5d065160
Shashank Ranjan <70715947+shashankranjan-dev@users.noreply.github.com>
2023-04-18 13:23:11 +0530

    Update Hero.js

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -6,7 +6,7 @@ export default function Index() {
   return (
 
     <div className="text-red-500 text-center text-xl hover:text-green-700 ">
-        <h1 className="font-semibold"> Hello i am vaibhav</h1>
+        <h1 className="font-semibold"> Hello i am Tech World</h1>
        
     </div>
  
